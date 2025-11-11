f7b5eacb090e2dfc958de643e7b309231d87745c
HUTANU LAURENTIU <laurentiu.hutanu@student.tuiasi.ro>
2025-11-11 19:03:54 +0200

    Almost done with the basic structure for client

--- client.py
+++ client.py
@@ -1,38 +1,89 @@
+import threading
+
 from textual.app import ComposeResult, App
-from textual.screen import Screen
+from textual.containers import Center, Vertical, Container
+from textual.screen import Screen, ModalScreen
 from textual import work
 from textual.widgets import Button, Input
 import socket
 
 HOST = "127.0.0.1"
-PORT = 2048
+PORT_TX = 6000
+PORT_RX = 5000
 
-class Send(Screen):
+class Send(ModalScreen):
+    BINDINGS = [("q", "request_quit", "Quit")]
+
+    def __init__(self):
+        super().__init__()
+        self.__stop_sending = threading.Event()
+        self.__stop_receiving = threading.Event()
+        self.rx_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.tx_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.rx_socket.bind((HOST, PORT_TX))
+
+
+    CSS_PATH = "send_client.tcss"
+
     def compose(self) -> ComposeResult:
-        yield Button("Send", variant="primary", id="send")
-        yield Input()
-
+        with Container(id="send-box"):
+            yield Button("Send", variant="primary", id="send")
+            yield Input()
 
     def on_button_pressed(self, event: Button.Pressed):
         if event.button.id == "send":
-            self.send_message(self.query_one(Input).value)
+            self.send(self.query_one(Input).value)
 
-    @work(thread=True)
-    async def send_message(self, data):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # The function is called when the user presses q button, because i setted it in bindings
+    def action_request_quit(self):
+        self.__stop_sending.set()
+        self.__stop_receiving.set()
+        self.app.exit()
+
+    def send(self, data):
         try:
-            s.sendto(bytes(data, encoding="ascii"), (HOST, PORT))
+            self.tx_socket.sendto(bytes(data, encoding="ascii"), (HOST, PORT_TX))
         finally:
-            s.close()
+            self.tx_socket.close()
+
+    def receive(self):
+        try:
+            data, addr = self.rx_socket.recvfrom(1024)
+        finally:
+            self.rx_socket.close()
+
+    @work(exclusive=True, thread=True)
+    def rx(self):
+        while not self.__stop_receiving.is_set():
+            self.rx_socket.settimeout(1)
+            try:
+                self.receive()
+            except socket.timeout:
+                continue
+
+
+    @work(exclusive=True, thread=True)
+    def tx(self):
+        data = "ceva"
+        while not self.__stop_sending.is_set():
+            self.tx_socket.settimeout(1)
+            try:
+                self.send(data)
+            except socket.timeout:
+                continue
+
 
 
 class ClientGUI(App):
-
-    def action_request_quit(self) -> None:
-        self.exit()
+    MODES = {
+        "send" : Send,
+    }
+    DEFAULT_MODE = 'send'
 
     def on_mount(self):
-        self.push_screen(Send())
+        self.push_screen("send")
+
+
 
 def main():
     ClientGUI().run()
--- server.py
+++ server.py
@@ -1,53 +1,72 @@
+import threading
 from random import randint
 
 from textual import events, work
 from textual.app import App, ComposeResult
-from textual.events import Print
+from textual.reactive import reactive
 from textual.screen import Screen, ModalScreen
 from textual.widgets import Label, Button, RichLog, Footer, Placeholder, Static
 from textual.containers import Grid, Horizontal, Container
-from asyncio import sleep
 import socket
 
-# async def on_input_changed(self, message: Input.Changed) -> None:
-#     """Called when the input changes"""
-#     # update_weather us a method defined by me
-#     self.run_worker(self.update_weather(message.value), exclusive=True)
-# """Sau adaug inainte de metoda  decoratorul   @work(exclusive=True)
-#     la metoda update_weather
-# """
-
 HOST = "127.0.0.1"
-PORT = 2048
+PORT_TX = 5000
+PORT_RX = 6000
 
-class StartServer(Screen):
+
+class StartServer(ModalScreen):
+    BINDINGS = [("q", "request_quit", "quit and back to the main menu")]
+    response = reactive("")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__stop_flag = threading.Event()
+
     def compose(self) -> ComposeResult:
-        yield Static("Result will appear here", id="result")
+        yield Button("Stop Server", id="stop")
+        yield Footer()
 
-    def on_mount(self) -> None:
-        placeholder = self.query_one("#result",Static)
-        self.receive_message(placeholder)
 
+    def on_button_pressed(self, event: Button.Pressed):
+        if event.button.id == "stop":
+            self.__stop_flag.set()
+            self.app.switch_mode("start")
+
+    def action_request_quit(self):
+        self.__stop_flag.set()
+        self.app.switch_mode("start")
 
     @work(thread=True)
-    async def receive_message(self, placeholder: Static):
+    async def receive_message(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-            s.bind((HOST, PORT))
-            self.call_later(placeholder.update, f"✅ Listening on {HOST}:{PORT}")
-            try:
-                data, addr = s.recvfrom(1024)
-                msg = data.decode("ascii")
-                self.call_later(
-                    placeholder.update,
-                    f"📩 Message from {addr[0]}:{addr[1]} → {msg}"
-                )
-                Print(f"Am receptionat: {data.decode("ascii")} de la {addr}")
-            except  Exception as e:
-                self.call_later(placeholder.update, f"Error: {e}")
+            self.__stop_flag.clear()
+            s.bind((HOST, PORT_RX))
+            s.settimeout(1)
+            self.app.call_from_thread(self.query_one("#result").update, f"✅ Listening on {HOST}:{PORT_RX}")
+            while not self.__stop_flag.is_set():
+                try:
+                    data, addr = s.recvfrom(1024)
+                    self.__stop_flag.set()
+                    msg = data.decode("ascii")
+                    self.app.call_from_thread(self.query_one("#result").update, f"Received message: {msg}")
+                except socket.timeout:
+                    continue
 
 
-class Start(Screen):
+
+class Settings(ModalScreen):
+    def compose(self):
+        yield Button("Yes", id="yes", variant="primary")
+        yield Footer()
+
+
+class Start(ModalScreen):
     CSS_PATH = "Start.tcss"
+    BINDINGS = [
+        ("s", "app.switch_mode('start_server')", "Start Server"),
+        ("t", "app.switch_mode('settings')", "Settings"),
+        ("q", "request_quit", "Quit")
+        ]
 
     def compose(self) -> ComposeResult:
         yield Horizontal(
@@ -58,22 +77,21 @@ class Start(Screen):
         )
         yield Footer()
 
+    def on_button_pressed(self, event: Button.Pressed):
+        if event.button.id == "start":
+            self.app.switch_mode('start_server')
+        elif event.button.id == "settings":
+            self.app.switch_mode('settings')
+        elif event.button.id == "quit":
+            self.app.exit()
 
     def action_request_quit(self) -> None:
         self.app.exit()
 
 
-class Settings(Screen):
-    def compose(self):
-        yield Button("Yes", id="yes", variant="primary")
-        yield Footer()
-
-    # def
 
 class ServerGUI(App):
     BINDINGS = [
-        ("s", "switch_mode('start_server')", "Start Server"),
-        ("t", "switch_mode('settings')", "Settings"),
         ("q", "request_quit", "Quit"),
     ]
     DEFAULT_MODE = 'start'
@@ -85,7 +103,6 @@ class ServerGUI(App):
 
     def action_request_quit(self) -> None:
         self.exit()
-
 
 
 def main():
